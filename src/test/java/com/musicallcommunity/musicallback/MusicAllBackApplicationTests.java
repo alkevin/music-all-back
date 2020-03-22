eba4d0a269a3ac431bdd7c9ba9852765fae8accb
Kévin Alves <kevin.alvespinto@gmail.com>
2020-03-22 20:49:36 +0100

    #FEATURE - init spring project (#7)
    * #Feature init collaboratif tools
    
    * #FEATURE - collaboration
    
    * #FEATURE - init spring project

---
+++ src/test/java/com/musicallcommunity/musicallback/MusicAllBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicallcommunity.musicallback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicAllBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
