55f892b007c8680eb7a05fc854fb615b151c613c
AKvin <kevin.alvespinto@gmail.com>
2020-03-22 16:43:45 +0100

    #FEATURE - init spring project

---
+++ src/main/java/com/musicallcommunity/musicallback/MusicAllBackApplication.java
@@ -0,0 +1,13 @@
+package com.musicallcommunity.musicallback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicAllBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicAllBackApplication.class, args);
+	}
+
+}
