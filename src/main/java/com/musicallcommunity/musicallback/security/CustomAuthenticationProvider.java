56f8bcdccce8fbab170d51af5f6fe4c07eb5ddf4
AKvin <kevin.alvespinto@gmail.com>
2020-05-14 00:02:08 +0200

    add custom authentication provider

---
+++ src/main/java/com/musicallcommunity/musicallback/security/CustomAuthenticationProvider.java
@@ -0,0 +1,40 @@
+package com.musicallcommunity.musicallback.security;
+
+import com.musicallcommunity.musicallback.model.User;
+import com.musicallcommunity.musicallback.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+public class CustomAuthenticationProvider extends DaoAuthenticationProvider {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public Authentication authenticate(Authentication auth) throws AuthenticationException {
+        final User user = userRepository.findByMail(auth.getName());
+        if ((user == null)) {
+            throw new BadCredentialsException("Invalid username or password");
+        }
+        final Authentication result = super.authenticate(auth);
+        return new UsernamePasswordAuthenticationToken(user, result.getCredentials(), result.getAuthorities());
+    }
+
+    private boolean isValidLong(String code) {
+        try {
+            Long.parseLong(code);
+        } catch (final NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
