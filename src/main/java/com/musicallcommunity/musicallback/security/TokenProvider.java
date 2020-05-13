8445cdb009a403cc79f8a889fc83dc70b3001b3e
AKvin <kevin.alvespinto@gmail.com>
2020-05-14 00:09:54 +0200

    add token provider

---
+++ src/main/java/com/musicallcommunity/musicallback/security/TokenProvider.java
@@ -0,0 +1,74 @@
+package com.musicallcommunity.musicallback.security;
+
+import com.musicallcommunity.musicallback.config.AppProperties;
+import com.musicallcommunity.musicallback.model.Role;
+import com.musicallcommunity.musicallback.model.User;
+import io.jsonwebtoken.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Service
+public class TokenProvider {
+
+    private static final Logger logger = LoggerFactory.getLogger(TokenProvider.class);
+    private AppProperties appProperties;
+    private final String ROLES_KEY = "roles";
+
+    public TokenProvider(AppProperties appProperties) {
+        this.appProperties = appProperties;
+    }
+
+    public String createToken(Authentication authentication, Collection<Role> roles) {
+        User userPrincipal = (User) authentication.getPrincipal();
+        Claims claims = Jwts.claims().setSubject(userPrincipal.getMail());
+
+        claims.put(ROLES_KEY, roles.stream().map(role -> new SimpleGrantedAuthority(role.getAuthority()))
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList()));
+
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + appProperties.getAuth().getJwtExpirationMsec());
+        logger.info("claims : " + claims);
+        return Jwts.builder()
+                .setSubject(userPrincipal.getMail())
+                .setIssuedAt(new Date())
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, appProperties.getAuth().getJwtSecret())
+                .compact();
+    }
+
+    public String getUserUsernameFromToken(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(appProperties.getAuth().getJwtSecret())
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
+    public boolean validateToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(appProperties.getAuth().getJwtSecret()).parseClaimsJws(authToken);
+            return true;
+        } catch (SignatureException ex) {
+            logger.error("Invalid JWT signature");
+        } catch (MalformedJwtException ex) {
+            logger.error("Invalid JWT token");
+        } catch (ExpiredJwtException ex) {
+            logger.error("Expired JWT token");
+        } catch (UnsupportedJwtException ex) {
+            logger.error("Unsupported JWT token");
+        } catch (IllegalArgumentException ex) {
+            logger.error("JWT claims string is empty.");
+        }
+        return false;
+    }
+}
