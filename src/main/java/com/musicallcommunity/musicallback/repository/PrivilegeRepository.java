2b8350d0f867cc55b05eebe85d4159d692826bd2
Kévin Alves <kevin.alvespinto@gmail.com>
2020-05-13 22:19:34 +0200

    #FEAT - Repository (#34)
    * #FEAT - swagger
    
    * add user model
    
    * add user dto
    
    * add auth provider and refactor model folder
    
    * add role model
    
    * add role name enum
    
    * add privilege model
    
    * add privilege name enum
    
    * add payload for signup request model
    
    * add email validator class
    
    * add password validator class
    
    * downgrade passay version to 1.0
    
    * add role dto
    
    * add privilege dto
    
    * update user dto
    
    * add user repository
    
    * add role repository
    
    * add privilege repository

---
+++ src/main/java/com/musicallcommunity/musicallback/repository/PrivilegeRepository.java
@@ -0,0 +1,14 @@
+package com.musicallcommunity.musicallback.repository;
+
+import com.musicallcommunity.musicallback.model.Privilege;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PrivilegeRepository extends JpaRepository<Privilege, Long> {
+
+    Privilege findByName(String name);
+
+    @Override
+    void delete(Privilege privilege);
+}
