ee3257c7466ddea73327b4267d13574f00b0d9d3
AKvin <kevin.alvespinto@gmail.com>
2020-05-13 22:14:40 +0200

    add role repository

---
+++ src/main/java/com/musicallcommunity/musicallback/repository/impl/RoleRepositoryImpl.java
@@ -0,0 +1,6 @@
+package com.musicallcommunity.musicallback.repository.impl;
+
+import com.musicallcommunity.musicallback.repository.RoleRepository;
+
+public abstract class RoleRepositoryImpl implements RoleRepository {
+}
