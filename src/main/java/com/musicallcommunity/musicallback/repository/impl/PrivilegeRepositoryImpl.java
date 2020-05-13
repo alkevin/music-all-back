5ab649a950cf37ed7368c47cf7b8f367bf17c4c2
AKvin <kevin.alvespinto@gmail.com>
2020-05-13 22:16:32 +0200

    add privilege repository

---
+++ src/main/java/com/musicallcommunity/musicallback/repository/impl/PrivilegeRepositoryImpl.java
@@ -0,0 +1,6 @@
+package com.musicallcommunity.musicallback.repository.impl;
+
+import com.musicallcommunity.musicallback.repository.PrivilegeRepository;
+
+public abstract class PrivilegeRepositoryImpl implements PrivilegeRepository {
+}
