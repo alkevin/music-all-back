d27e8698e2e333f0f6583fe620fe04a1d4528b20
AKvin <kevin.alvespinto@gmail.com>
2020-05-17 18:23:28 +0200

    add exceptions

---
+++ src/main/java/com/musicallcommunity/musicallback/exception/InvalidOldPasswordException.java
@@ -0,0 +1,17 @@
+package com.musicallcommunity.musicallback;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class InvalidOldPasswordException extends RuntimeException {
+    private String resourceName;
+    private String fieldName;
+
+    public InvalidOldPasswordException(String resourceName, String fieldName) {
+        super(String.format("%s doesn't matche %s", resourceName, fieldName));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+    }
+
+}
